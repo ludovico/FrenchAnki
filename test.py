f0a7d81959f549c3778d26dfbdb1519023bbdc48
vidar <vbskretting@tutanota.com>
2021-08-12 22:23:47 +0200

    Fungerende med flere ordklasser

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ sample = samples.get_text()
 
 # This is our string:
 string = html2text.html2text(sample)
+originalstring = string
 
 # String cleaning:
 # Clear linebreaks
@@ -39,16 +40,36 @@ phrases_to_remove.append("transitive verb: Verb taking a direct object--for exam
 phrases_to_remove.append("verbal expression: Phrase with special meaning functioning as verb--for example, DOUBLE2put their heads together,DOUBLE2 DOUBLE2come to an end.DOUBLE2")
 phrases_to_remove.append("verbe pronominal: verbe qui sSINGLE1utilise avec le pronom réfléchi DOUBLE2seDOUBLE2, qui sSINGLE1accorde avec le sujet. Ex : se regarder : DOUBLE2Je me regarde dans le miroir. Tu te regardes dans le miroir.DOUBLE2. Les verbes pronominaux se conjuguent toujours avec lSINGLE1auxiliaire DOUBLE2êtreDOUBLE2. Ex : DOUBLE2Elle a lavé la voitureDOUBLE2 mais DOUBLE2Elle sSINGLE1est lavée.DOUBLE2")
 phrases_to_remove.append(": Verb with adverb(s) or preposition(s), having special meaning, divisible--for example, DOUBLE2call offDOUBLE2 [=cancel], DOUBLE2call the game off,DOUBLE2 DOUBLE2call off the game.DOUBLE2")
+phrases_to_remove.append("nmnom masculin: sSINGLE1utilise avec les articles DOUBLE2leDOUBLE2, DOUBLE2lSINGLE1DOUBLE2 (devant une voyelle ou un h muet), DOUBLE2unDOUBLE2. Ex : garçon - nm > On dira DOUBLE2le garçonDOUBLE2 ou DOUBLE2un garçonDOUBLE2. ")
+phrases_to_remove.append("nnoun: Refers to person, place, thing, quality, etc. ")
+phrases_to_remove.append("adjadjectif: modifie un nom. Il est généralement placé après le nom et sSINGLE1accorde avec le nom (ex : un ballon bleu, une balle bleue). En général, seule la forme au masculin singulier est donnée. Pour former le féminin, on ajoute DOUBLE2eDOUBLE2 (ex : petit > petite) et pour former le pluriel, on ajoute DOUBLE2sDOUBLE2 (ex : petit > petits). Pour les formes qui sont DOUBLE2irrégulièresDOUBLE2 au féminin, celles-ci sont données (ex : irrégulier, irrégulière > irrégulier = forme masculine, irrégulière = forme féminine)")
+phrases_to_remove.append("adjadjectif: modifie un nom. Il est généralement placé après le nom et sSINGLE1accorde avec le nom (ex : un ballon bleu, une balle bleue). En général, seule la forme au masculin singulier est donnée. Pour former le féminin, on ajoute DOUBLE2eDOUBLE2 (ex : petit > petite) et pour former le pluriel, on ajoute DOUBLE2sDOUBLE2 (ex : petit > petits). Pour les formes qui sont DOUBLE2irrégulièresDOUBLE2 au féminin, celles- ci sont données (ex : irrégulier, irrégulière > irrégulier = forme masculine, irrégulière = forme féminine)")
+phrases_to_remove.append("adjadjective: Describes a noun or pronoun--for example, DOUBLE2a tall girl,DOUBLE2 DOUBLE2an interesting book,DOUBLE2 DOUBLE2a big house.DOUBLE2 ")
+phrases_to_remove.append("adjadjective: Describes a noun or pronoun-- for example, DOUBLE2a tall girl,DOUBLE2 DOUBLE2an interesting book,DOUBLE2 DOUBLE2a big house.DOUBLE2 ")
+phrases_to_remove.append("nfnom féminin: sSINGLE1utilise avec les articles DOUBLE2laDOUBLE2, DOUBLE2lSINGLE1DOUBLE2 (devant une voyelle ou un h muet), DOUBLE2uneDOUBLE2. Ex : fille - nf > On dira DOUBLE2la filleDOUBLE2 ou DOUBLE2une filleDOUBLE2. Avec un nom féminin, lSINGLE1adjectif sSINGLE1accorde. En général, on ajoute un DOUBLE2eDOUBLE2 à lSINGLE1adjectif. Par exemple, on dira DOUBLE2une petite filleDOUBLE2. ")
+phrases_to_remove.append("nfnom féminin: sSINGLE1utilise avec les articles DOUBLE2laDOUBLE2, DOUBLE2lSINGLE1DOUBLE2 (devant une voyelle ou un h muet), DOUBLE2uneDOUBLE2. Ex : fille - nf > On dira DOUBLE2la filleDOUBLE2 ou DOUBLE2une filleDOUBLE2. Avec un nom féminin, lSINGLE1adjectif sSINGLE1accorde. En général, on ajoute un DOUBLE2eDOUBLE2 à lSINGLE1adjectif. Par exemple, on dira DOUBLE2une petite filleDOUBLE2. ")
 
 for i in phrases_to_remove:
     while string.find(i) != -1:
-        string = string.replace(i, "")
+        string = string.replace(i, "ENDPART")
 
 # Managing quotation marks again:
 while string.find("DOUBLE2") != -1:
     string = string.replace("DOUBLE2", '"')
 while string.find("SINGLE1") != -1:
     string = string.replace("SINGLE1", "'")
+
+# Other things to remove:
+phrases_to_remove = []
+phrases_to_remove.append("ⓘCette phrase n'est pas une traduction de la phrase originale.")
+phrases_to_remove.append("Un oubli important ? Signalez une erreur ou suggérez une amélioration. Traductions supplémentairesFrançaisAnglais")
+
+
+for i in phrases_to_remove:
+    while string.find(i) != -1:
+        string = string.replace(i, "")
+
+print(string)
 
 # Finding definitions:
 next = "y"
@@ -59,32 +80,74 @@ while next == "y":
     definition = string[(definition_start +1):(definition_end)]
     print("French Definition: " + definition + "\n")
     # English Definition:
-    eng_def_end = string.find("⇒", definition_end)
     eng_def_start = definition_end +1
+    eng_def_end = string.find("ENDPART", definition_end)
     eng_def = string[eng_def_start:eng_def_end]
+    if eng_def.find("⇒") != -1:
+        eng_def = eng_def[0:(eng_def.find("⇒"))]
+    next_eng_def_start = eng_def_end + 7
+    next_eng_def_end = string.find("ENDPART", next_eng_def_start)
+    next_search = string[next_eng_def_start:next_eng_def_end]
+    check_dots = re.search('[.!?]', next_search)
+    while check_dots == None:
+        eng_def_new = (string[(next_eng_def_start):next_eng_def_end])
+        eng_def = eng_def + ", " + eng_def_new
+        next_eng_def_start = next_eng_def_end +7
+        next_eng_def_end = string.find("ENDPART", next_eng_def_start)
+        next_search = string[next_eng_def_start:next_eng_def_end]
+        check_dots = re.search('[.!?]', next_search)
+        eng_def = eng_def.replace(" ,", ",")
     print("English Definition: " + eng_def + "\n")
     # French Sentence:
-    string = string[eng_def_end:(len(string))] # Kutter strengen for å gjøre det enklere med regex
-    capital = re.search('[A-ZÀ-Ÿ]', string).span()
-    sentence_start = capital[0]
-    symbol = re.search('[.!?]', string).span()
-    sentence_end = symbol[0] +1
-    sentence = string[sentence_start:sentence_end]
-    print("French Sentence: " + sentence + "\n")
+    # Finding end of sentence, checking if there are more than one sentence for each language
+    string = string[next_eng_def_start:len(string)]
+    sentence_end = string.find("ENDPART", 0)
+    search = string[0:sentence_end]
+    find_all_dots = re.findall('[.!?]', search)
+    number_sentences = len(find_all_dots)/2
+    if find_all_dots == 3:
+        iterator = 1
+    else:
+        iterator = number_sentences
+    fre_sen_start = 0
+    number = 1
+    while iterator != 0 and re.search('[.!?]', search) != None:
+        fre_sen_end = re.search('[.!?]', search).span()
+        fre_sen_end_result = re.search('[.!?]', search)
+        fre_sen_end = fre_sen_end[0] +1
+        fre_sen = search[fre_sen_start:fre_sen_end]
+        print("French Sentence " + str(number) + ": " + fre_sen + "\n")
+        number = number +1
+        search = search.replace(fre_sen, "")
+        iterator = iterator -1
+        # French Sentence: Remove keyword:
+        keyword = inp[0:(len(inp)-2)]
+        if fre_sen.find(keyword) != 1:
+            remove_word_start = fre_sen.find(keyword)
+            remove_word_end = fre_sen.find(" ", remove_word_start)
+            remove_word = fre_sen[remove_word_start:remove_word_end]
+            sentence_keyword_removed = fre_sen.replace(remove_word, "___")
+            print("Sentence with keyword removed: " +  sentence_keyword_removed + "\n")
+        else: print("Keyword not found")
     #English sentence
-    string = string[(symbol[0]+1):len(string)]
-    symbol = re.search('[.!?]', string).span()
-    english_sentence_end = symbol[0]
-    english_sentence = string[1:(english_sentence_end+1)]
-    print("English Sentence: " + english_sentence + "\n")
-    # French Sentence: Remove keyword:
-    keyword = inp[0:(len(inp)-2)]
-    if sentence.find(keyword) != 1:
-        remove_word_start = sentence.find(keyword)
-        remove_word_end = sentence.find(" ", remove_word_start)
-        remove_word = sentence[remove_word_start:remove_word_end]
-        sentence_keyword_removed = sentence.replace(remove_word, "___")
-        print("Sentence with keyword removed: " +  sentence_keyword_removed + "\n")
-    else: print("Keyword not found")
-    string = string[(string.find(".", sentence_end+1)+1):len(string)]
+    if find_all_dots == 3:
+        iterator = 1
+    else:
+        iterator = number_sentences
+    number = 1
+    eng_sen_start = 0
+    eng_sen_end = 0
+    while iterator != 0 and re.search('[.!?]', search) != None:
+        eng_sen_end = re.search('[.!?]', search).span()
+        eng_sen_end_result = re.search('[.!?]', search)
+        eng_sen_end = eng_sen_end[0] +1
+        eng_sen = search[eng_sen_start:eng_sen_end]
+        print("English Sentence " + str(number) + ": " + eng_sen + "\n")
+        number = number +1
+        search = search.replace(eng_sen, "")
+        iterator = iterator -1
+    if eng_sen_end != 0:
+        string = string[(string.find("ENDPART", eng_sen_end)+1):len(string)]
+    else:
+        string = string[(string.find("ENDPART", fre_sen_end)+1):len(string)]
     next = input("Do you want to scroll to the next definition? Press y to scroll or any key to continue")
